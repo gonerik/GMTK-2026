b33d86c52d462e4dfcaad83112391d46462e5033
s28398 <64951198+SystemCall213@users.noreply.github.com>
2026-07-24 17:11:07 +0200

    camera petri dishes switch

---
+++ GMTK-2026/Assets/Scripts/CameraControl/CameraSwitchInstaller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Cinemachine;
+using System.Collections.Generic;
+using CameraControl;
+using Zenject;
+
+public class CameraSwitchInstaller : MonoInstaller
+{
+    [SerializeField] private CinemachineBrain brain;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<CameraResolver>().AsSingle();
+        Container.Bind<CinemachineBrain>().FromInstance(brain).AsSingle();
+        Container.BindInterfacesAndSelfTo<CameraZoomController>().AsSingle().NonLazy();
+    }
+    
+}
