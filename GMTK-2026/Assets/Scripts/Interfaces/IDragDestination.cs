1496fdcb88057fb3187b2cc3535ce22158f25181
s28398 <64951198+SystemCall213@users.noreply.github.com>
2026-07-25 22:37:37 +0200

    syringe almost done

--- GMTK-2026/Assets/Scripts/Interfaces/IDragDestination.cs
+++ GMTK-2026/Assets/Scripts/Interfaces/IDragDestination.cs
@@ -1,7 +1,10 @@
-﻿namespace Interfaces
+﻿using UnityEngine;
+
+namespace Interfaces
 {
     public interface IDragDestination
     {
         public void ExecuteDrag(IDragable dragable);
+        public void ExecuteSpawn(GameObject prefab, Vector3 position);
     }
 }
---
+++ GMTK-2026/Assets/Scripts/Syringe/SyringeCooldown.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace.Syringe
+{
+    public class SyringeCooldown : MonoBehaviour
+    {
+        [SerializeField] private Image cooldownImage;
+        private bool _isCooldown;
+
+        public bool IsCooldown => _isCooldown;
+
+        private void Awake()
+        {
+            if (cooldownImage == null)
+            {
+                cooldownImage = GetComponent<Image>();
+            }
+
+            if (cooldownImage != null)
+            {
+                cooldownImage.type = Image.Type.Filled;
+                cooldownImage.fillMethod = Image.FillMethod.Radial360;
+                cooldownImage.fillAmount = 0;
+                cooldownImage.enabled = false;
+            }
+        }
+
+        public void StartCooldown(float duration)
+        {
+            if (_isCooldown) return;
+            CooldownRoutine(duration).Forget();
+        }
+
+        private async UniTaskVoid CooldownRoutine(float duration)
+        {
+            _isCooldown = true;
+            float elapsed = 0;
+
+            if (cooldownImage != null)
+            {
+                cooldownImage.enabled = true;
+                cooldownImage.fillAmount = 1;
+                while (elapsed < duration)
+                {
+                    elapsed += Time.deltaTime;
+                    cooldownImage.fillAmount = 1 - (elapsed / duration);
+                    await UniTask.Yield(PlayerLoopTiming.Update, this.GetCancellationTokenOnDestroy());
+                }
+                cooldownImage.fillAmount = 0;
+                cooldownImage.enabled = false;
+            }
+            else
+            {
+                await UniTask.Delay(System.TimeSpan.FromSeconds(duration), cancellationToken: this.GetCancellationTokenOnDestroy());
+            }
+
+            _isCooldown = false;
+        }
+    }
+}
