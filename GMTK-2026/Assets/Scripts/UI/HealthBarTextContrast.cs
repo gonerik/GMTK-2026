c30e2249b3bed14dedca72459e767ffb9334ca1a
Mykola-Ihnatenko <koliiaihk@gmail.com>
2026-07-25 19:56:00 +0200

    [Scene][UI] Main menu and Final scene added. HUD implemented.

---
+++ GMTK-2026/Assets/Scripts/UI/HealthBarTextContrast.cs
@@ -0,0 +1,43 @@
+using Energy;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI
+{
+    public class HealthBarTextContrast : MonoBehaviour
+    {
+        [SerializeField] private Image healthBarFill;       // BarBackground
+        [SerializeField] private RectTransform maskFilled;   // TextMask_Filled (bottom)
+        [SerializeField] private RectTransform maskEmpty;    // TextMask_Empty (top)
+        [SerializeField] private TMP_Text textOnFilled;      // black text
+        [SerializeField] private TMP_Text textOnEmpty;       // white text
+        [SerializeField] private float containerHeight = 100f;
+        [Inject] public EnergyService energyService;
+        
+        private void Awake()
+        {
+            //image = GetComponent<Image>();
+            energyService.OnEnergyChanged += OnEnergyChanged;
+        } 
+        public void OnDestroy()
+        {
+            energyService.OnEnergyChanged -= OnEnergyChanged;
+        }
+
+        private void OnEnergyChanged(int percent01)
+        {
+            healthBarFill.fillAmount = percent01;
+
+            string label = percent01 + "%";
+            textOnFilled.text = label;
+            textOnEmpty.text = label;
+
+            float filledHeight = containerHeight - percent01;
+
+            maskFilled.sizeDelta = new Vector2(maskFilled.sizeDelta.x, containerHeight - filledHeight);
+            maskEmpty.sizeDelta = new Vector2(maskEmpty.sizeDelta.x, filledHeight);
+        }
+    }
+}
