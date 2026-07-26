ecdb67a978a41ed0c4b80616c542a4978a641882
Mykola-Ihnatenko <koliiaihk@gmail.com>
2026-07-26 16:20:51 +0200

    Main menu

--- GMTK-2026/Assets/Scripts/UI/MainMenuController.cs
+++ GMTK-2026/Assets/Scripts/UI/MainMenuController.cs
@@ -1,5 +1,6 @@
 ﻿using CoreLoop.Interfaces;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Zenject;
@@ -17,6 +18,8 @@ namespace UI
         [SerializeField] private Button settingsButton;
         [SerializeField] private Button creditsButton;
         [SerializeField] private Button quitButton;
+        
+        [SerializeField] private PlayableDirector director;
 
         private void OnEnable()
         {
@@ -36,7 +39,8 @@ namespace UI
 
         public void Play()
         { 
-            SceneManager.LoadScene("Final");
+            director.Play();
+            //SceneManager.LoadScene("Final");
         }
         
         public void Settings()
