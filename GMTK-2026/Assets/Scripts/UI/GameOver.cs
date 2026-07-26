c4198dd251af6e44ce8d27ce65ff11467e047c5f
s28398 <64951198+SystemCall213@users.noreply.github.com>
2026-07-26 18:17:56 +0200

    game over prefab

---
+++ GMTK-2026/Assets/Scripts/UI/GameOver.cs
@@ -0,0 +1,39 @@
+﻿using Energy;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UI
+{
+    public class GameOver : MonoBehaviour
+    {
+        [Inject] private SignalBus _signalBus;
+        
+        [SerializeField] private Button restartButton;
+        [SerializeField] private GameObject panel;
+        
+        private void OnEnable()
+        {
+            _signalBus.Subscribe<EnergyService.OnEnergyLostSignal>(OnGameOver);
+            restartButton.onClick.AddListener(RestartGame);
+        }
+
+        private void OnDisable()
+        {
+            _signalBus.Unsubscribe<EnergyService.OnEnergyLostSignal>(OnGameOver);
+            restartButton.onClick.RemoveListener(RestartGame);
+        }
+
+        private void OnGameOver()
+        {
+            panel.SetActive(true);
+        }
+
+        private void RestartGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
