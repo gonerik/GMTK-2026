478b9dda09d5e2f9c36dedd73cbc92a6b2222665
gonerik <163130399+gonerik@users.noreply.github.com>
2026-07-26 18:09:33 +0200

    MusicSwitcher

---
+++ GMTK-2026/Assets/Scripts/Audio/MainGameMusicSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Audio
+{
+    public class MainGameMusicSwitcher: MonoBehaviour
+    {
+        [Inject] MusicService musicService;
+
+        private void Start()
+        {
+            musicService.PlayGameMusic();
+        }
+    }
+}
